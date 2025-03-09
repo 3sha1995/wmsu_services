598f9e6f9f44ba8d5ca07b28f23ed060f173b568
Trisha <trishafaye004@gmail.com>
2025-03-09 22:51:16 +0800

    adjustment for the css and adding the footer

--- page/scholarship.php
+++ page/scholarship.php
@@ -7,4 +7,7 @@
 <body>
     <?php include'../__includes/navbar.php'; ?>  
     <?php include'../__includes/scholarship.php'; ?>
+    <?php
+    require_once '../__includes/footer.php';
+    ?>
 </body>
